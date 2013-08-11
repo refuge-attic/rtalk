4aa2384a44b013662c5418dd53021730e88be1b9
benoitc <bchesneau@gmail.com>
2013-08-11 18:00:34 +0200

    fix indent

--- src/lib/session.js
+++ src/lib/session.js
@@ -18,8 +18,8 @@ Session.prototype._init = function(password, salt) {
 	// calculate box and sign keypairs from the password and salt
 	// TODO: make scrypt settings configurable
 	key8bytes = scrypt.crypto_scrypt(scrypt.encode_utf8(password),
-																	scrypt.encode_utf8(salt),
-																	16384, 8, 1, 64);
+			scrypt.encode_utf8(salt),
+			16384, 8, 1, 64);
 
 	this._box_keypair = nacl.crypto_box_keypair_from_seed(key8bytes);
 	this._sign_keypair = nacl.crypto_sign_keypair_from_seed(
@@ -30,12 +30,12 @@ Session.prototype._init_id = function() {
 
 	// 1. concatenate box pub key and sign pub key
 	var id = nacl.to_hex(this._box_keypair.boxPk) +
-						 nacl.to_hex(this._sign_keypair.signPk);
+		nacl.to_hex(this._sign_keypair.signPk);
 
-  // 2. peform sha512 on spub and perform ripemd 160  hash on it
+	// 2. peform sha512 on spub and perform ripemd 160  hash on it
 	id = CryptoJS.RIPEMD160(nacl.to_hex(nacl.crypto_hash_string(id)),
-															  {asBytes: true})
-															  .toString();
+			{asBytes: true})
+	.toString();
 
 	// 3. add version to the ripe
 	id = this.version + id;
@@ -47,7 +47,7 @@ Session.prototype._init_id = function() {
 	checksum = checksum.subarray(0, 3);
 
 	// 6. append the ckecksum to the id
-  id = id + nacl.to_hex(checksum);
+	id = id + nacl.to_hex(checksum);
 
 	// 7. encode to base58
 	id = base58.encode(hex2bytes(id));
